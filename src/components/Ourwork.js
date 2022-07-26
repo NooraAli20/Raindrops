7285a6731aa6d022faa996a4d96287ff022bc72c
NooraAli20 <70133983+NooraAli20@users.noreply.github.com>
2022-07-26 21:55:42 +0200

    Add files via upload

---
+++ src/components/Ourwork.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Ourwork = () => {
+  return (
+    <div className='ourwork'>
+        <div className='left'>
+            <h1>Our current projects</h1>
+            <p>We are currently working on educating people in how to safely store their rainwater. This project was established due to the increasing drought in the region, that leads to many people dying of thirst.</p>
+        </div>
+        <div className='right'>
+          <div className='gif'></div>
+        </div>
+    </div>
+  )
+}
+
+export default Ourwork
